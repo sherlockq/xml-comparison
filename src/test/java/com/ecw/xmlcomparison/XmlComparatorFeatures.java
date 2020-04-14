05fc66a63bb068f6cbf4f1503c82fecf5d641c6b
SirXOPHER <chris.reinmuller@gmail.com>
2020-04-14 17:50:04 +0100

    Add more tests for XML comparison

--- src/test/java/com/ecw/xmlcomparison/XmlComparatorFeatures.java
+++ src/test/java/com/ecw/xmlcomparison/XmlComparatorFeatures.java
@@ -1,16 +1,17 @@
 package com.ecw.xmlcomparison;
 
-import jdk.nashorn.api.scripting.ScriptObjectMirror;
 import org.junit.jupiter.api.Test;
 import org.xmlunit.assertj.XmlAssert;
 import org.xmlunit.builder.DiffBuilder;
 import org.xmlunit.builder.Input;
+import org.xmlunit.diff.DefaultNodeMatcher;
 import org.xmlunit.diff.Diff;
 import org.xmlunit.diff.Difference;
+import org.xmlunit.diff.ElementSelectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class XmlComparatorFeatures {
+class XmlComparatorFeatures {
     @Test
     void compareIdenticalDocuments() {
         final String control = "<a><b attr=\"abc\"></b></a>";
@@ -54,6 +55,7 @@ public class XmlComparatorFeatures {
 
         assertThat(diff.hasDifferences()).as(diff.toString()).isFalse();
     }
+
     @Test
     void compareTwoFilesWithAssertXml() {
         Diff diff = DiffBuilder
@@ -93,5 +95,110 @@ public class XmlComparatorFeatures {
         XmlAssert.assertThat(test).and(control).areNotIdentical();
         XmlAssert.assertThat(test).and(control).areSimilar();
         XmlAssert.assertThat(test).and(control).areNotSimilar();
+    }
+
+    @Test
+    void documents_considered_identical_with_different_attribute_order() {
+        //language=XML
+        String original = "<typeId extension=\"POCD_HD000040\" root=\"2.16.840.1.113883.1.3\"/>";
+        //language=XML
+        String swapped = "<typeId root=\"2.16.840.1.113883.1.3\" extension=\"POCD_HD000040\"/>";
+
+        XmlAssert.assertThat(original).and(swapped).areIdentical();
+    }
+
+    @Test
+    void documents_considered_identical_with_different_order_of_same_tag_elements() {
+        //language=XML
+        String original = "<root>\n" +
+                "<templateId root=\"2.16.840.1.113883.10.20.22.1.2\" extension=\"2015-08-01\"/>\n" +
+                "<templateId root=\"2.16.840.1.113883.10.20.22.1.2\"/>\n" +
+                "</root>";
+
+        //language=XML
+        String swapped = "<root>\n" +
+                "<templateId root=\"2.16.840.1.113883.10.20.22.1.2\"/>\n" +
+                "<templateId root=\"2.16.840.1.113883.10.20.22.1.2\" extension=\"2015-08-01\"/>\n" +
+                "</root>";
+
+        XmlAssert.assertThat(original).and(swapped)
+                .ignoreChildNodesOrder()
+                .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.byNameAndAllAttributes))
+                .areIdentical();
+    }
+
+    @Test
+    void documents_considered_identical_with_different_order_of_individual_tag_elements() {
+        //language=XML
+        String original = "<addr use=\"HP\">\n" +
+                "<streetAddressLine>2222 Home Street</streetAddressLine>\n" +
+                "<city>Beaverton</city>\n" +
+                "<state>OR</state>\n" +
+                "<postalCode>97867</postalCode>\n" +
+                "<country>US</country>\n" +
+                "</addr>";
+
+        //language=XML
+        String swapped = "<addr use=\"HP\">\n" +
+                "<streetAddressLine>2222 Home Street</streetAddressLine>\n" +
+                "<country>US</country>\n" +
+                "<city>Beaverton</city>\n" +
+                "<state>OR</state>\n" +
+                "<postalCode>97867</postalCode>\n" +
+                "</addr>";
+
+        XmlAssert.assertThat(original).and(swapped)
+                .ignoreChildNodesOrder()
+                .areIdentical();
+    }
+
+    @Test
+    void documents_considered_identical_with_different_order_of_sections() {
+        //language=XML
+        String original =
+                "<structuredBody>\n" +
+                "<component>\n" +
+                "        <section>\n" +
+                "            <!-- *** Allergies and Intolerances Section (entries required) (V3) *** -->\n" +
+                "            <templateId root=\"2.16.840.1.113883.10.20.22.2.6.1\" extension=\"2015-08-01\"/>\n" +
+                "            <templateId root=\"2.16.840.1.113883.10.20.22.2.6.1\"/>\n" +
+                "        </section>\n" +
+                "    " +
+                "</component>\n" +
+                "    " +
+                "<component>\n" +
+                "        <section>\n" +
+                "            <!-- *** Encounters section (entries required) (V3) *** -->\n" +
+                "            <templateId root=\"2.16.840.1.113883.10.20.22.2.22.1\" extension=\"2015-08-01\"/>\n" +
+                "            <templateId root=\"2.16.840.1.113883.10.20.22.2.22.1\"/>\n" +
+                "        </section>\n" +
+                "    " +
+                "</component>\n" +
+                "</structuredBody>";
+
+        //language=XML
+        String swapped =
+                "<structuredBody>\n" +
+                        "<component>\n" +
+                        "        <section>\n" +
+                        "            <!-- *** Encounters section (entries required) (V3) *** -->\n" +
+                        "            <templateId root=\"2.16.840.1.113883.10.20.22.2.22.1\" extension=\"2015-08-01\"/>\n" +
+                        "            <templateId root=\"2.16.840.1.113883.10.20.22.2.22.1\"/>\n" +
+                        "        </section>\n" +
+                        "    " +
+                        "</component>\n" +
+                        "    <component>\n" +
+                        "        <section>\n" +
+                        "            <!-- *** Allergies and Intolerances Section (entries required) (V3) *** -->\n" +
+                        "            <templateId root=\"2.16.840.1.113883.10.20.22.2.6.1\" extension=\"2015-08-01\"/>\n" +
+                        "            <templateId root=\"2.16.840.1.113883.10.20.22.2.6.1\"/>\n" +
+                        "        </section>\n" +
+                        "    </component>\n" +
+                        "</structuredBody>";
+
+        XmlAssert.assertThat(original).and(swapped)
+                .ignoreChildNodesOrder()
+                .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.byXPath(".//templateId", ElementSelectors.byNameAndAllAttributes)))
+                .areIdentical();
     }
 }
