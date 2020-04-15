58a03ed50c5ab44f4d9e1e3615bd98b195e36558
Zhiqiang (Sherlock) Qiao <615526+sherlockq@users.noreply.github.com>
2020-04-15 16:29:29 +0100

    Clean up test cases and use a universal CCDA comparator.

--- src/test/java/com/ecw/xmlcomparison/XmlComparatorFeatures.java
+++ src/test/java/com/ecw/xmlcomparison/XmlComparatorFeatures.java
@@ -9,89 +9,24 @@ import org.xmlunit.diff.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class XmlComparatorFeatures {
-    @Test
-    void compareIdenticalDocuments() {
-        final String control = "<a><b attr=\"abc\"></b></a>";
-        final String test = "<a><b attr=\"xyc\"></b></a>";
-
-        Diff myDiff = DiffBuilder.compare(Input.fromString(control))
-                .withTest(Input.fromString(test))
-                .build();
-
-//        myDiff.getDifferences().iterator().next().
-        assertThat(myDiff.hasDifferences()).as(myDiff.toString()).isFalse();
-    }
 
     @Test
     void compareTwoFiles() {
-        Diff diff = DiffBuilder
 
-                .compare(Input.fromURL(XmlComparatorFeatures.class.getResource("/sample-1.xml")))
-                .withTest(Input.fromURL(XmlComparatorFeatures.class.getResource("/sample-2.xml")))
+        Diff diff = generateCcdaDiff(Input.fromURL(XmlComparatorFeatures.class.getResource("/sample-1.xml"))
+                , Input.fromURL(XmlComparatorFeatures.class.getResource("/sample-2.xml")));
 
-                .checkForSimilar().checkForIdentical() // [1]
-                .ignoreComments() // [2]
-                .ignoreWhitespace() // [3]
-                .normalizeWhitespace() // [4]
-//                .withComparisonController(ComparisonController) // [5]
-//                .withComparisonFormatter(comparisonFormatter) // [6]
-//                .withComparisonListeners(comparisonListeners) // [7]
-//                .withDifferenceEvaluator(differenceEvaluator) // [8]
-//                .withDifferenceListeners(comparisonListeners) // [9]
-//                .withNodeMatcher(nodeMatcher) // [10]
-//                .withAttributeFilter(attributeFilter) // [11]
-//                .withNodeFilter(nodeFilter) // [12]
-//                .withNamespaceContext(map) // [13]
-//                .withDocumentBuilerFactory(factory) // [14]
-//                .ignoreElementContentWhitespace() // [15]
-
-                .build();
         for (Difference difference : diff.getDifferences()) {
             System.out.println(difference.toString());
         }
 
-        assertThat(diff.hasDifferences()).as(diff.toString()).isFalse();
+        assertThat(diff.hasDifferences()).as(diff.toString()).isTrue();
     }
 
     @Test
-    void compareTwoFilesWithAssertXml() {
-        Diff diff = DiffBuilder
-
-                .compare(Input.fromURL(XmlComparatorFeatures.class.getResource("/sample-1.xml")))
-                .withTest(Input.fromURL(XmlComparatorFeatures.class.getResource("/sample-2.xml")))
-
-                .checkForSimilar().checkForIdentical() // [1]
-                .ignoreComments() // [2]
-                .ignoreWhitespace() // [3]
-                .normalizeWhitespace() // [4]
-//                .withComparisonController(ComparisonController) // [5]
-//                .withComparisonFormatter(comparisonFormatter) // [6]
-//                .withComparisonListeners(comparisonListeners) // [7]
-//                .withDifferenceEvaluator(differenceEvaluator) // [8]
-//                .withDifferenceListeners(comparisonListeners) // [9]
-//                .withNodeMatcher(nodeMatcher) // [10]
-//                .withAttributeFilter(attributeFilter) // [11]
-//                .withNodeFilter(nodeFilter) // [12]
-//                .withNamespaceContext(map) // [13]
-//                .withDocumentBuilerFactory(factory) // [14]
-//                .ignoreElementContentWhitespace() // [15]
-
-                .build();
-
+    void documents_considered_not_identical() {
         XmlAssert.assertThat(Input.fromURL(XmlComparatorFeatures.class.getResource("/sample-1.xml")))
-                .and(Input.fromURL(XmlComparatorFeatures.class.getResource("/sample-2.xml"))).areIdentical();
-    }
-
-    // Not working, didn't show "Click to see differences"
-    @Test
-    void testWithXmlUnitSamples() {
-        final String control = "<a><b attr=\"abc\"></b></a>";
-        final String test = "<a><b attr=\"xyz\"></b></a>";
-
-        XmlAssert.assertThat(test).and(control).areIdentical();
-        XmlAssert.assertThat(test).and(control).areNotIdentical();
-        XmlAssert.assertThat(test).and(control).areSimilar();
-        XmlAssert.assertThat(test).and(control).areNotSimilar();
+                .and(Input.fromURL(XmlComparatorFeatures.class.getResource("/sample-2.xml"))).areNotIdentical();
     }
 
     @Test
@@ -101,7 +36,8 @@ class XmlComparatorFeatures {
         //language=XML
         String swapped = "<typeId root=\"2.16.840.1.113883.1.3\" extension=\"POCD_HD000040\"/>";
 
-        XmlAssert.assertThat(original).and(swapped).areIdentical();
+        Diff diff = generateCcdaDiff(Input.fromString(original), Input.fromString(swapped));
+        assertThat(diff.hasDifferences()).as(diff.toString()).isFalse();
     }
 
     @Test
@@ -118,10 +54,8 @@ class XmlComparatorFeatures {
                 "<templateId root=\"2.16.840.1.113883.10.20.22.1.2\" extension=\"2015-08-01\"/>\n" +
                 "</root>";
 
-        XmlAssert.assertThat(original).and(swapped)
-                .ignoreChildNodesOrder()
-                .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.byNameAndAllAttributes))
-                .areIdentical();
+        Diff diff = generateCcdaDiff(Input.fromString(original), Input.fromString(swapped));
+        assertThat(diff.hasDifferences()).as(diff.toString()).isFalse();
     }
 
     @Test
@@ -144,9 +78,8 @@ class XmlComparatorFeatures {
                 "<postalCode>97867</postalCode>\n" +
                 "</addr>";
 
-        XmlAssert.assertThat(original).and(swapped)
-                .ignoreChildNodesOrder()
-                .areIdentical();
+        Diff diff = generateCcdaDiff(Input.fromString(original), Input.fromString(swapped));
+        assertThat(diff.hasDifferences()).as(diff.toString()).isFalse();
     }
 
     @Test
@@ -154,24 +87,24 @@ class XmlComparatorFeatures {
         //language=XML
         String original =
                 "<structuredBody>\n" +
-                "<component>\n" +
-                "        <section>\n" +
-                "            <!-- *** Allergies and Intolerances Section (entries required) (V3) *** -->\n" +
-                "            <templateId root=\"2.16.840.1.113883.10.20.22.2.6.1\" extension=\"2015-08-01\"/>\n" +
-                "            <templateId root=\"2.16.840.1.113883.10.20.22.2.6.1\"/>\n" +
-                "        </section>\n" +
-                "    " +
-                "</component>\n" +
-                "    " +
-                "<component>\n" +
-                "        <section>\n" +
-                "            <!-- *** Encounters section (entries required) (V3) *** -->\n" +
-                "            <templateId root=\"2.16.840.1.113883.10.20.22.2.22.1\" extension=\"2015-08-01\"/>\n" +
-                "            <templateId root=\"2.16.840.1.113883.10.20.22.2.22.1\"/>\n" +
-                "        </section>\n" +
-                "    " +
-                "</component>\n" +
-                "</structuredBody>";
+                        "<component>\n" +
+                        "        <section>\n" +
+                        "            <!-- *** Allergies and Intolerances Section (entries required) (V3) *** -->\n" +
+                        "            <templateId root=\"2.16.840.1.113883.10.20.22.2.6.1\" extension=\"2015-08-01\"/>\n" +
+                        "            <templateId root=\"2.16.840.1.113883.10.20.22.2.6.1\"/>\n" +
+                        "        </section>\n" +
+                        "    " +
+                        "</component>\n" +
+                        "    " +
+                        "<component>\n" +
+                        "        <section>\n" +
+                        "            <!-- *** Encounters section (entries required) (V3) *** -->\n" +
+                        "            <templateId root=\"2.16.840.1.113883.10.20.22.2.22.1\" extension=\"2015-08-01\"/>\n" +
+                        "            <templateId root=\"2.16.840.1.113883.10.20.22.2.22.1\"/>\n" +
+                        "        </section>\n" +
+                        "    " +
+                        "</component>\n" +
+                        "</structuredBody>";
 
         //language=XML
         String swapped =
@@ -193,12 +126,10 @@ class XmlComparatorFeatures {
                         "    </component>\n" +
                         "</structuredBody>";
 
-        XmlAssert.assertThat(original).and(swapped)
-                .ignoreChildNodesOrder()
-                .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.byXPath(".//templateId", ElementSelectors.byNameAndAllAttributes)))
-                .areIdentical();
+        Diff diff = generateCcdaDiff(Input.fromString(original), Input.fromString(swapped));
+        assertThat(diff.hasDifferences()).as(diff.toString()).isFalse();
     }
-    
+
     @Test
     void documents_considered_identical_with_different_order_of_nested_id_tags() {
         //language=XML
@@ -216,18 +147,26 @@ class XmlComparatorFeatures {
                 "    </section>\n" +
                 "</parent>";
 
+        Diff diff = generateCcdaDiff(Input.fromString(original), Input.fromString(swapped));
+        assertThat(diff.hasDifferences()).as(diff.toString()).isFalse();
+    }
+
+    Diff generateCcdaDiff(Input.Builder control, Input.Builder test) {
+        ElementSelector templateIdSelector = ElementSelectors.byXPath("./templateId", ElementSelectors.byNameAndAllAttributes);
+        ElementSelector componentTemplateIdSelector = ElementSelectors.byXPath("./section/templateId", ElementSelectors.byNameAndAllAttributes);
         ElementSelector elementSelector = ElementSelectors.conditionalBuilder()
-                .whenElementIsNamed("section").thenUse(ElementSelectors.byXPath("./templateId", ElementSelectors.byNameAndAllAttributes))
+                .whenElementIsNamed("section").thenUse(templateIdSelector)
+                .whenElementIsNamed("component").thenUse(componentTemplateIdSelector)
+                .whenElementIsNamed("ClinicalDocument").thenUse(ElementSelectors.byName)
                 .elseUse(ElementSelectors.byNameAndAllAttributes)
                 .build();
 
-//        assertThat(diff.isIdentical())
-        XmlAssert.assertThat(original).and(swapped)
+        return DiffBuilder.compare(control).withTest(test)
                 .ignoreComments()
                 .ignoreWhitespace()
-                .ignoreChildNodesOrder()
                 .withNodeMatcher(new DefaultNodeMatcher(elementSelector))
-                .areIdentical();
+                // difference in child order is considered a similarity
+                .checkForSimilar()
+                .build();
     }
-
 }
